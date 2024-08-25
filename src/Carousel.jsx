c52ac2bc27c7a21f46241e6e54e32088527d4241
Nazirul <nazirulrocky@gmail.com>
2024-08-26 03:08:34 +0800

    carousel

---
+++ src/Carousel.jsx
@@ -0,0 +1,135 @@
+import {Row, Col, Container,Button,Collapse, Fade} from 'react-bootstrap';
+import { motion, AnimatePresence, useAnimationControls } from "framer-motion";
+import { useState } from 'react';
+
+const Carousel = ({ images,  }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [click, setClick] = useState(false);
+  const [direction, setDirection] = useState(null);
+
+  const controls = useAnimationControls();
+
+  const nextSlide = () => {
+    setDirection("right");
+    controls.start('rightEnter');
+    setActiveIndex((prevIndex) => 
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    )
+  };
+
+  const prevSlide = () => {
+    setDirection("left")
+    setActiveIndex((prevIndex) => 
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1  
+    )
+  };
+
+  // !-------------------------------------------
+
+  const testimonies = [
+    {
+      id: 1,
+      name: 'Slide 1',
+      comment: 'Comment 1',
+    },
+    {
+      id: 0,
+      name: 'Slide 2',
+      comment: 'Comment 2',
+    }
+  ]
+
+  const ContainerTestimonies = ({name, comment}) => (
+    <div>
+        <p>{name}</p>
+        <p>{comment}</p>
+    </div>
+  )
+  // ! CONTINUE CAROUSEL
+  const variants = {
+    rightEnter: {x: 0},
+    rightExit: {x : -1000},
+
+
+    leftEnter: {x: 0},
+    leftExit: {x : 10},
+
+    visible: {opacity: 1}
+  }
+
+  const handleClick = () => {
+    controls.start('rightEnter')
+    console.log(click)
+  }
+
+
+
+const imgCarousel =     
+  <AnimatePresence>
+    <motion.div
+      onClick={handleClick}
+      variants = {variants}
+      initial = {'rightExit'}
+      animate = { controls } 
+      transition = {{duration: .2}}
+      exit={{opacity: 0}}
+    >    
+
+      <img
+        src={images[activeIndex]}
+        alt={`Slide ${activeIndex}`}
+        className='carousel_img'
+        style={{width: "100px"}}
+      />
+        
+  </motion.div>
+  </AnimatePresence>
+
+
+
+const buttons = 
+  <>
+    <button onClick={prevSlide} className='carousel_btn carousel_btn--prev'>
+    &lt;</button>
+
+    <button onClick={nextSlide} className='carousel_btn carousel_btn--next'>
+    &gt;</button>
+  </>
+
+
+
+  const sliderVariants = {
+    hiddenRight : {
+      x: "100%",
+      opacity: 0
+    },
+    initial: {
+      y: 0,
+    },
+  }
+
+
+
+  return ( 
+    <Container className="carousel">
+        <Row>
+          <h1>TESTIMONIALS</h1>
+          <Col>
+
+            {imgCarousel}
+
+
+            <ContainerTestimonies
+              name={testimonies[activeIndex].name}
+              comment={testimonies[activeIndex].comment}
+            />
+            {buttons}
+          </Col>
+        </Row>
+
+
+      </Container>
+   )
+}
+ 
+export default Carousel;
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,37 +1,22 @@
 import {Row, Col, Container,Button,Collapse, Fade} from 'react-bootstrap';
+import Carousel from './Carousel';
 
 const Contact = () => {
 
 
-  const testimonies = [
-    {
-      id: 1,
-      name: 'megat',
-      comment: 'I am megat',
-      pic: 'src/images/pp.webp'
-    },
-    {
-      id: 2,
-      name: 'eyra',
-      comment: 'I am eyra',
-      pic: 'src/images/pp.webp'
-    }
-  ]
+  
 
-  const ContainerTestimonies = ({name, comment, pic}) => (
-    <Col>
-      <div>
-        <img src={pic} alt={name} style={{width: "100px"}}/>
-        <p>{name}</p>
-        <p>{comment}</p>
-      </div>
-  </Col>
-  )
+  const images = [
+    'src/images/pp.webp',
+    'src/images/me.jpg'
+  ]
   
   return ( 
     <Container fluid>
+
+    <Carousel images={images}/>
       
-      <Row>
+      {/* <Row>
         <h1>TESTIMONIALS</h1>
         <Col>
           {testimonies.map((test) => (
@@ -43,7 +28,7 @@ const Contact = () => {
               />  
           ))}
         </Col>
-      </Row>
+      </Row> */}
 
     </Container>
    );
