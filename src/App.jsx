0f90db64262eb786aa042c5f06d14a30d8a127db
Nazirul <142727276+Sovietroll@users.noreply.github.com>
2024-09-17 23:19:26 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,7 @@ useEffect(() => {
       
       <NavBar {...{toggleDarkMode, isDarkMode}} />
       <Routes>
-        <Route path="/nazirulrocky" element = 
+        <Route path="/" element = 
         {<Home 
           {...{windowWidth,buttonRef,isClicked, setIsClicked,isClickedTwo,setIsClickedTwo,toggleDarkMode, isDarkMode}}
           />} 
